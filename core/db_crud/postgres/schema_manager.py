40db793cd37078f6a5e253929b3714238bd18e77
cybrvybe <alexfigueroa.cybr@gmail.com>
2023-05-26 22:42:40 -0700

    Adds core module and placeholder classes

---
+++ core/db_crud/postgres/schema_manager.py
@@ -0,0 +1,12 @@
+class SchemaManager:
+    def create_schema(self, schema_name):
+        return None
+    
+    def update_schema(self, schema_name):
+        return None
+    
+    def scan_schema_structure(self, schema_name):
+        return None
+    
+    def delete_schema(self, schema_name):
+        return None
---
+++ core/db_crud/postgres/server_manager.py
@@ -0,0 +1,9 @@
+class ServerManager:
+    def create_server(self, host_name, server_name, port, username, password):
+        return None
+    
+    def delete_server(self, server_name, password):
+        return None
+    
+    def update_server(self, host_name, server_name, port, username, password):
+        return None
