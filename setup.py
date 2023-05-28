bfc28b1d28fa8e831fee70b38d82bcf0942947a7
cybrvybe <alexfigueroa.cybr@gmail.com>
2023-05-27 23:41:40 -0700

    adds requirements.txt handling in setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup
 
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+
 setup(
     name="nexus-db",
     version="0.1",
     packages=["nexus_db"],
+    install_requires=requirements,
     entry_points={
         "console_scripts": [
             "nexus=nexus_db.cli.main:main",
